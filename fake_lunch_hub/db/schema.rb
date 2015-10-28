308f5bf32fc157a86519ad4e602ced8387e035e2
David P. Hanesana <dhanesana@gmail.com>
2015-10-27 23:20:04 -0700

    Create and migrate :groups table

--- fake_lunch_hub/db/schema.rb
+++ fake_lunch_hub/db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151028061359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "groups", ["name"], name: "index_groups_on_name", unique: true, using: :btree
 
 end
